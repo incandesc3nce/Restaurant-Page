86d534ea86a363fc8959e2aebd63e213ce03ad8a
incandesc3nce <62383010+incandesc3nce@users.noreply.github.com>
2024-06-18 19:24:43 +0700

    feat: implement navigation through nav buttons

--- src/index.js
+++ src/index.js
@@ -1,4 +1,48 @@
 import { fillHomeContent } from "./content-modules/home";
+import { fillMenuContent } from "./content-modules/menu";
 import './style.css';
 
+const buttonHandler = (function() {
+  const homeBtn = document.querySelector('#home');
+  const menuBtn = document.querySelector('#menu');
+  const aboutBtn = document.querySelector('#about');
+  
+  const getActiveButton = () => {return document.querySelector('.btn-active')};
+
+  const toggleActiveButton = () => {
+    const activeButton = getActiveButton();
+    activeButton.classList.remove('btn-active');
+    activeButton.classList.add('btn-inactive');
+  };
+
+  const removeChildren = () => {
+    const content = document.querySelector('#content');
+    while (content.firstChild) {
+      content.removeChild(content.firstChild);
+    }
+  };
+
+  const addListeners = () => {
+    homeBtn.addEventListener('click', () => {
+      toggleActiveButton();
+      homeBtn.classList.add('btn-active');
+      homeBtn.classList.remove('btn-inactive');
+      
+      removeChildren();
+      fillHomeContent();
+    });
+    menuBtn.addEventListener('click', () => {
+      toggleActiveButton();
+      menuBtn.classList.add('btn-active');
+      menuBtn.classList.remove('btn-inactive');
+  
+      removeChildren();
+      fillMenuContent();
+    });
+  };
+
+  return { addListeners };
+})(); 
+
 fillHomeContent();
+buttonHandler.addListeners();
